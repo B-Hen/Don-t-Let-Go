ea3fb61e5162100b3a19b8e85cfff39b759c1524
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2022-02-24 09:38:36 -0500

    FixProjectName

---
+++ DontLetGo/Assets/Scripts/RestartGame.cs
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////
+//Developer: Breanna Henriquez 
+//Date: 2/21/2022
+//Purpose: Button to restart game
+//Edited by:
+//Edit Date:
+//////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
